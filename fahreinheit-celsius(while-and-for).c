ef260ce6be51f105b5d13de5cd2bc79065461b68
flakvard <marni@falkvard.com>
2022-09-26 17:13:51 +0200

    refactoring

--- fahreinheit-celsius(while-and-for).c
+++ fahreinheit-celsius(while-and-for).c
@@ -3,6 +3,8 @@
 /* print Fahrenheit-Celsius table
 for f = 0, 20, ..., 300 */
 
+float FahrToCels(float fahr);
+
 main()
 {
     /*Using a while loop to print out Fahrenheit-Celsius table */
@@ -14,17 +16,29 @@ main()
     fahr = lower; 
 
     while (fahr <= upper){
-        celsius = (5.0/9.0) * (fahr-32.0);
+        celsius = FahrToCels(fahr);
         printf("%4.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
-
     printf(" \n");
+    
 
+    
+    
     /*Using a for loop to print out Fahrenheit-Celsius table */
+    /*
     int fah;
 
-    for (fah = 0; fah <= 300; fah = fah + 20){
+    for (fah = lower; fah <= upper; fah = fah + step){
         printf("%4d %6.1f\n", fah, (5.0/9.0)*(fah-32));
     }
+    */
+   return 0;
 }   
+
+float FahrToCels(float fahr){
+    
+    float celsius;
+    celsius = (5.0/9.0) * (fahr-32.0);
+    return celsius;
+}
